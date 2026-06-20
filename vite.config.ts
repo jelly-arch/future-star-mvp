a06707d601754e6c5a2d7d32153b784ebd0593a5
jelly-arch <jelly-arch@users.noreply.github.com>
2026-06-20 05:50:32 +0000

    Fix: set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/future-star-mvp/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
